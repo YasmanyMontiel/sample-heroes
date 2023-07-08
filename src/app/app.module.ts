3b554bdb9f4e9c0f0d0572e5ab2140ecc417c5d5
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2023-07-08 15:51:33 +0200

    Generating hero component with: ng n c name-componen and ng g c name-componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './heroe/heroe.component';
+import { AllHeroesComponent } from './heroe/components/all-heroes/all-heroes.component';
+import { UpadteHeroeComponent } from './heroe/components/upadte-heroe/upadte-heroe.component';
+import { DeleteHeroeComponent } from './heroe/components/delete-heroe/delete-heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroeComponent
+    HeroeComponent,
+    AllHeroesComponent,
+    UpadteHeroeComponent,
+    DeleteHeroeComponent
   ],
   imports: [
     BrowserModule,
